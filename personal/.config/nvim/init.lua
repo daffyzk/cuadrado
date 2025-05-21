ba26399e928e410fcdeee12be84c08ac1ee196af
Daffy Hellman <main@mgc.sh>
2025-05-21 11:07:09 -0300

    updated personal

--- personal/.config/nvim/init.lua
+++ personal/.config/nvim/init.lua
@@ -9,7 +9,6 @@ vim.wo.relativenumber = true
 vim.g.mapleader = " "
 
 -- lazy package manager
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -24,35 +23,81 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    {"nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = {"nvim-lua/plenary.nvim"}},
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x", 
+    {"nvim-telescope/telescope.nvim", 
+        tag = "0.1.5", 
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        }
+    },                                  -- fuzzy-find and live-grep files
+    {"nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate"},           -- syntax highlighting
+    {"nvim-neo-tree/neo-tree.nvim", 
+        branch = "v3.x", 
         dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim"
         }
-    },
+    },                                  -- cool side bar for files
     {"neovim/nvim-lspconfig"},
-    {"tpope/vim-fugitive"},
-    {"askfiy/visual_studio_code"}
+    {"hrsh7th/nvim-cmp"},               -- autocomplete
+    {"hrsh7th/cmp-buffer"},
+    {"hrsh7th/cmp-path"},
+    {"hrsh7th/cmp-nvim-lua"},
+    {"hrsh7th/cmp-nvim-lsp"},
+    {"noir-lang/noir-nvim"},            -- noir lsp
+    {"tpope/vim-fugitive"},             -- git goodness 
+    {"askfiy/visual_studio_code"}       -- vscode theme
 }
 
 local opts = {}
 
+-- lazy package manager
 require("lazy").setup(plugins,opts)
+local telescope = require("telescope.builtin")
+local treesitter = require("nvim-treesitter.configs")
+local cmp = require("cmp")
 
--- telescope
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ["C-d"] = cmp.mapping.scroll_docs(-4), 
+        ["C-f"] = cmp.mapping.scroll_docs(4),
+        ["C-Space"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-y>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+        }),
+    }),
+    snippet = {
+        expand = function(args)
+            vim.snippet.expand(args.body)
+            vim.snippet.expand()
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    sources = {
+        { name = "nvim_lua" },
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "buffer", keyboard_length = 5 },
+    },
+})
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+-- cmp.setup.cmdline(":", {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = cmp.config.sources({
+--         {name = "path" }
+--     }, {
+--         {name = "cmdline"}
+--        }),
+--     matching = { disallow_symbol_nonprefix_matching = false }
+-- })
 
--- treesitter 
-
-local config = require("nvim-treesitter.configs")
-
-config.setup({
+treesitter.setup({
     ensure_installed = {
         "lua", "rust", "go", "javascript", "typescript", "python"
     },
@@ -60,12 +105,7 @@ config.setup({
     indent = { enable = true },
 })
 
--- neo-tree (hell yeah)
-
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
-
 -- LSP memes
-
 local handlers = {
     ["textDocument/publishDiagnostics"] = vim.lsp.with(
         vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -128,7 +168,29 @@ local function lsp_jump_in_tab(method)
   vim.lsp.buf[method:match("([^/]+)$")]()
 end
 
+-- SHORTCUTS -- shortcuts -- SHORTCUTS -- shortcuts -- SHORTCUTS -- 
 
+-- to move in tabs on the default layer
+vim.keymap.set('n', '<Leader>j', vim.cmd.tabfirst)
+vim.keymap.set('n', '<Leader>k', vim.cmd.tabnext) 
+vim.keymap.set('n', '<Leader>l', vim.cmd.tabprev) 
+vim.keymap.set('n', '<Leader>;', vim.cmd.tablast) 
+vim.keymap.set('n', '<Leader>n', function () return ':tabnew ' end, { expr = true })
+
+-- telescope
+vim.keymap.set("n", "<C-p>", telescope.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
+
+-- neo-tree
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
+
+-- autocomplete (I would put it here if i had one)
+
+
+-- find and replace (a.k.a. rename)
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+
+-- diagnostics and code actions
 local on_attach = function(client, bufnr)
     -- vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev vim.diagnostic.open_float end)
     -- vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next vim.diagnostic.open_float end)
@@ -151,6 +213,7 @@ local config = {
     handlers = handlers,
 }
 
+-- my forced 4 tab spacing breaks ts formatting so here's a fix
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "typescript",
     callback = function()
